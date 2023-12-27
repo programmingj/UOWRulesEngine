66e83fd81b458bbaeb67c30f914fb1a8b79bce68
programmingj@gmail.com <programmingj@gmail.com>
2023-12-27 07:08:20 -0700

    Removed the outdated UOWRulesEngine project because the code used an unsupported .Net version and was replaced by the UOWRulesEngineCore project.

---
+++ UOWRulesEngineCore/Attributes/UOWAttributeBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UOWRulesEngineCore.Attributes
+{
+	/// <summary>
+	/// Base class for Attributes implemented using the UOWRulesEngineCore library.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class)]
+	public class UOWAttributeBase : Attribute
+	{
+	}
+}
